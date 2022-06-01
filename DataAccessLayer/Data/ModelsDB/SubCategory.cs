478dc541fb4b5c8f8e44260548a69b86e5ce4141
aliidiiab <ali-diab97@hotmail.com>
2022-06-01 15:17:55 +0200

    PCGaming,Monitors,Accessories ADDED

--- DataAccessLayer/Data/ModelsDB/SubCategory.cs
+++ DataAccessLayer/Data/ModelsDB/SubCategory.cs
@@ -8,11 +8,31 @@ namespace DataAccessLayer.Data.ModelsDB
 {
     public class SubCategory
     {
+        public SubCategory()
+        {
+            Laptops = new HashSet<Laptop>();
+            PCGamings = new HashSet<PCGaming>();
+            Monitors = new HashSet<Monitor>();
+            Accessories = new HashSet<Accessories>();
+
+
+        }
         public Guid ID { get; set; }
         public string? Name { get; set; }
 
         public Guid CategoryID { get; set; }
 
         public virtual Category Category { get; set; }
+
+
+        public virtual ICollection<Laptop> Laptops { get; set; }
+        public virtual ICollection<PCGaming> PCGamings { get; set; }
+        public virtual ICollection<Monitor> Monitors { get; set; }
+        public virtual ICollection<Accessories> Accessories { get; set; }
+
+
+
+
+
     }
 }
