219856971e4154965561c28da36e678257332a81
AhmedIsmail113 <ahmedismail40000@gmail.com>
2022-06-01 15:46:20 +0200

    Mobiles,Tablets

--- DataAccessLayer/Data/ModelsDB/SubCategory.cs
+++ DataAccessLayer/Data/ModelsDB/SubCategory.cs
@@ -8,11 +8,18 @@ namespace DataAccessLayer.Data.ModelsDB
 {
     public class SubCategory
     {
+        public SubCategory()
+        {
+            MobilePhones = new HashSet<MobilePhone>();
+            Tablets = new HashSet<Tablet>();
+        }
         public Guid ID { get; set; }
         public string? Name { get; set; }
 
         public Guid CategoryID { get; set; }
 
         public virtual Category Category { get; set; }
+        public virtual ICollection<MobilePhone> MobilePhones { get; set; }
+        public virtual ICollection<Tablet> Tablets { get; set; }
     }
 }
