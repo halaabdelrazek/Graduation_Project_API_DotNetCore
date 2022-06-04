980d16c2fe2ca3b1d7e032bb483b72acf89c94c8
AhmedIsmail113 <ahmedismail40000@gmail.com>
2022-06-04 18:46:34 +0200

    Updating DataLayer

--- DataAccessLayer/Data/ModelsDB/Order.cs
+++ DataAccessLayer/Data/ModelsDB/Order.cs
@@ -8,6 +8,21 @@ namespace DataAccessLayer.Data.ModelsDB
 {
     public class Order
     {
+        public Order()
+        {
+            Laptops = new HashSet<Laptop>();
+            PCGamings = new HashSet<PCGaming>();
+            Monitors = new HashSet<Monitor>();
+            Accessories = new HashSet<Accessories>();
+            CardioEquipments = new HashSet<CardioEquipment>();
+            SportsSupplements = new HashSet<SportsSupplements>();
+            ShoesSports = new HashSet<ShoesSport>();
+
+            Perfume = new HashSet<Perfume>();
+            MakeUp = new HashSet<MakeUp>();
+            MobilePhones = new HashSet<MobilePhone>();
+            Tablets = new HashSet<Tablet>();
+        }
         public Guid Id { get; set; }
         public decimal ShippingPrice { get; set; }
         public decimal Tax { get; set; }
@@ -21,6 +36,21 @@ namespace DataAccessLayer.Data.ModelsDB
         public Guid CarrierId { get; set; }
 
         public Carrier Carrier { get; set; }
+
+        public virtual ICollection<Laptop> Laptops { get; set; }
+        public virtual ICollection<PCGaming> PCGamings { get; set; }
+        public virtual ICollection<Monitor> Monitors { get; set; }
+        public virtual ICollection<Accessories> Accessories { get; set; }
+        public virtual ICollection<MakeUp> MakeUp { get; set; }
+        public virtual ICollection<Perfume> Perfume { get; set; }
+
+        public virtual ICollection<CardioEquipment> CardioEquipments { get; set; }
+        public virtual ICollection<SportsSupplements> SportsSupplements { get; set; }
+
+        public virtual ICollection<ShoesSport> ShoesSports { get; set; }
+
+        public virtual ICollection<MobilePhone> MobilePhones { get; set; }
+        public virtual ICollection<Tablet> Tablets { get; set; }
 
 
 
